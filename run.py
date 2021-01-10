e9b7d2797039ba7f37d71936983ad6100c606de5
smoonmare <smoonmare@gmail.com>
2021-01-10 14:11:34 -0800

    Added Sentiment score plot. Added data aggregation line plot.

--- run.py
+++ run.py
@@ -25,20 +25,16 @@ import plotly.express as px
 sns.set(style='darkgrid')
 
 df = pd.read_csv('https://raw.githubusercontent.com/gabrielpreda/covid-19-tweets/master/covid19_tweets.csv') # df - data frame
-# df.head()
-# Shape of the dataframe
-# df.shape
+# df.head() # Show dataframe
+# df.shape # Shape of the dataframe
 
 needed_columns = ['user_name','date','text']
 df = df[needed_columns]
-# df.head()
 
 # Changing category of the column
 df.user_name = df.user_name.astype('category')
 df.user_name = df.user_name.cat.codes # Assigns unique numerrical value for each unique user name
-
 df.date = pd.to_datetime(df.date).dt.date
-# df.head()
 
 # Tweet's texts
 texts = df['text']
@@ -83,3 +79,16 @@ sentiment_df = pd.DataFrame(data = list(sentiment_scores))
 # Labeling the scores based on the compound polarity value
 labelize = lambda x: 'neutral' if x == 0 else('positive' if x > 0 else 'negative')
 sentiment_df['label'] = sentiment_df.compound.apply(labelize)
+
+# Data frames joining
+data = df.join(sentiment_df.label)
+
+# Plotting the sentiment score counts
+counts_df = data.label.value_counts().reset_index()
+counts_df.columns = ['Index', 'Label']
+# sns.barplot(x='Index', y='Label', data=counts_df) # Barplot for sentiment counts
+
+# Data aggregation
+data_agg = data[['user_name', 'date', 'label']].groupby(['date', 'label']).count().reset_index()
+data_agg.columns = ['Date', 'Label', 'Counts']
+px.line(data_agg, x='Date', y='Counts', color='Label', title='Daily Tweets Sentimental Analysis')
